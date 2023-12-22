130c7b3b7a5334f852c368e6c9e8137f32183714
OXM474 <lijixin1997@gmail.com>
2023-12-22 10:08:57 +0630

    fouth commit

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const tourRouter = require("./router/tour_router");
 const userRouter = require("./router/user_router");
+const morgan = require("morgan");
+const middleware = require("./middlewares/logger");
+
 const app = express();
+
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(middleware.logger);
 
 app.use("/callapi/0.1/tours", tourRouter);
 app.use("/callapi/0.1/auth", userRouter);
+app.use("/callapi/0.1/user", userRouter);
 
 const port = process.env.port || 3000;
 app.listen(port, () => console.log(`Listening at Port ${port}...`));
